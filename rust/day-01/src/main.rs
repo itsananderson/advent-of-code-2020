37843c074da19f45495c6429b81a9e3a3f008977
Will Anderson <will@itsananderson.com>
2020-12-02 12:49:29 -0800

    Update gitignore and stub out day 1 puzzle

---
+++ rust/day-01/src/main.rs
@@ -0,0 +1,13 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+
+    println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
+}
